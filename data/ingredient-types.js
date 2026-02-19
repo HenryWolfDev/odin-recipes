963db8e78fad0eac6e49275c1913585df9e06b87
Henry <HWolf.binz@gmail.com>
2026-02-20 00:57:30 +0100

    (chore): fix recipe object structure (chore): change html element for recipe loading to button element

--- data/ingredient-types.js
+++ data/ingredient-types.js
@@ -1,1 +1,47 @@
-export const ingredientsTypes = {};
+export const ingredientsTypes = {
+  german: {
+    Rind: 'minced-meat.png',
+    Hähnchen: 'chicken.png',
+
+    Zwiebel: 'onions.png',
+    Knoblauch: 'garlic.png',
+    Kidneybohnen: 'beans.png',
+    Tomaten: 'tomatoes.png',
+    Ingwer: 'ginger.png',
+
+    Joghurt: 'creme.png',
+    Sahne: 'creme.png',
+    Ei: 'egg.png',
+
+    Samen: 'seeds.png',
+
+    Gewürze: 'spices.png',
+    Kräuter: 'herbs.png',
+
+    Öl: 'oil.png',
+    Mehl: 'flour.png',
+  },
+
+  english: {
+    Beef: 'minced-meat.png',
+    Chicken: 'chicken.png',
+
+    Onion: 'onions.png',
+    Garlic: 'garlic.png',
+    KidneyBeans: 'beans.png',
+    Tomatoes: 'tomatoes.png',
+    Ginger: 'ginger.png',
+
+    Yogurt: 'creme.png',
+    Cream: 'creme.png',
+    Egg: 'egg.png',
+
+    Seeds: 'seeds.png',
+
+    Spices: 'spices.png',
+    Herbs: 'herbs.png',
+
+    Oil: 'oil.png',
+    Flour: 'flour.png',
+  },
+};
